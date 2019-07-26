e5dedc1831b47e603d5cfdd671b8f898b48be97b
kanji0028 <bukurabo@gmail.com>
2019-07-26 19:45:12 +0900

    Created Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    Post.create(price:post_params[:price], category:post_params[:category], mental:post_params[:mental], memo:post_params[:memo])
+    redirect_to controller: :posts, action: :index
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:price, :category, :mental, :memo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'posts#index'
-  get  'posts'     => 'posts#index'
-  get  'posts/new' => 'posts#new'  #新規投稿
+  root  'posts#index'
+  get   'posts'     => 'posts#index'
+  get   'posts/new' => 'posts#new'     #新規入力画面
+  post  'posts'     => 'posts#create'  #新規入力機能
 end
