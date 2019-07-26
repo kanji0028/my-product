66cd8cde0606abb6a83b30467199650a13c1e7c9
kanji0028 <bukurabo@gmail.com>
2019-07-26 17:20:55 +0900

    Post Database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'posts' => 'posts#index'
+  root 'posts#index'
+  get 'posts'     => 'posts#index'
+  get 'posts/new' => 'posts#new'  #新規投稿
 end
