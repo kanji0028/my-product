6886e57ae6718fbae24de2039b8f73a5fb4fc75b
kanji0028 <bukurabo@gmail.com>
2019-07-31 12:19:49 +0900

    emotion %

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,19 +6,6 @@ class PostsController < ApplicationController
     @my_posts  = current_user.posts.includes(:user).order("created_at DESC") 
     @other_posts  = Post.where.not(user_id: current_user.id).includes(:user).order("created_at DESC")
 
-    @sum = Post.where(user_id: current_user.id).sum(:price) #合計
-    @this_month_sum = Post.where(user_id: current_user.id, created_at: Time.now.all_month).sum(:price)  # 今月
-    @last_month_sum = Post.where(user_id: current_user.id, created_at: Time.now.ago(1.month)).sum(:price)  #先月
-    
-    @food = Post.where(user_id: current_user.id).where(category: 'restaurant').sum(:price)
-    @cart = Post.where(user_id: current_user.id).where(category: 'shopping_cart').sum(:price)
-    @train = Post.where(user_id: current_user.id).where(category: 'train').sum(:price)
-    @import_contacts = Post.where(user_id: current_user.id).where(category: 'import_contacts').sum(:price)
-    @card_travel = Post.where(user_id: current_user.id).where(category: 'card_travel').sum(:price)
-    @bikes = Post.where(user_id: current_user.id).where(category: 'directions_bike').sum(:price)
-    @payment = Post.where(user_id: current_user.id).where(category: 'payment').sum(:price)
-    @star = Post.where(user_id: current_user.id).where(category: 'star').sum(:price)
-
     @pie_chart = Post.where(user_id: current_user.id).group(:category).sum(:price)
     @line_chart = Post.where(user_id: current_user.id).group(:created_at).count
 
