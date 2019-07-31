a7a76713ce5974e20ba7125da5191fe025196d21
kanji0028 <bukurabo@gmail.com>
2019-07-31 14:33:24 +0900

    Memo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,15 @@ class PostsController < ApplicationController
   def index
     @my_posts  = current_user.posts.includes(:user).order("created_at DESC") 
     @other_posts  = Post.where.not(user_id: current_user.id).includes(:user).order("created_at DESC")
+    @posts  = Post.order("created_at DESC")
+
+    @good = Post.where(mental:'sentiment_very_satisfied').count.to_f / Post.count('mental').to_f * 100
+    @normal = Post.where(mental:'sentiment_neutral').count.to_f / Post.count('mental').to_f * 100
+    @bad = Post.where(mental:'sentiment_very_dissatisfied').count.to_f / Post.count('mental').to_f * 100
 
     @pie_chart = Post.where(user_id: current_user.id).group(:category).sum(:price)
     @line_chart = Post.where(user_id: current_user.id).group(:created_at).count
+
 
   end
 
