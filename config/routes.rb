d98fa32ce29bd128fcda89a4d3e7a4450d45c25e
kanji0028 <bukurabo@gmail.com>
2019-08-01 18:41:02 +0900

    memo detail

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root  'posts#index'
 
-  resources :users, only: [:show] 
+  resources :users, only: [:show, :index] 
   resources :posts, except: [:show] do
      resources :likes, only: [:create, :destroy]
   end
