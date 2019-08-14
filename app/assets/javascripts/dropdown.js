e8208e743586626c8b2eff4a7833f6b47ee9276d
kanji0028 <bukurabo@gmail.com>
2019-08-14 23:22:53 +0900

    JS Sweet2

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -3,7 +3,6 @@ $(document).on('turbolinks:load',function(){
     $('.dropdown').hide();
     
     $('img').hover(function(){
-      debugger;
       $(".dropdown",).fadeIn();
     }, function(){
       $(".dropdown").fadeOut();
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -25,34 +25,7 @@ $(function() {
     }
 });
 
-// (?)削除ボタンモーダル画面
-$(function() {  
-  $('.post__top--list.aaa').on('click', function() {
-    $('#overlay, #modalWindow').fadeIn();
-  });
-  
-  $('#close1').on('click', function() {
-    $('#overlay, #modalWindow').fadeOut();
-  });
-  
-  locateCenter();
-  $(window).resize(locateCenter);
-
-  function locateCenter() {
-    let w = $(window).width();
-    let h = $(window).height();
-    
-    let cw = $('#modalWindow').outerWidth();
-    let ch = $('#modalWindow').outerHeight();
-   
-    $('#modalWindow').css({
-      'left': ((w - cw) / 2) + 'px',
-      'top': ((h - ch) / 2) + 'px'
-    });
-  }
-});
-
-$(function(){
+$(document).on('turbolinks:load',function(){
   $("i:contains('sentiment_very_satisfied')").addClass("blue");
   $("i:contains('sentiment_neutral')").addClass("green");
   $("i:contains('sentiment_very_dissatisfied')").addClass("red");
