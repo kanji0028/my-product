507f087a8be67d94abcc2c92d4e0b93832fc71e5
kanji0028 <bukurabo@gmail.com>
2019-09-10 19:31:15 +0900

    Edit Array

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,8 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:show ,:index]
 
-
   def show
-    @my_posts   = current_user.posts.order("created_at DESC") 
+    @my_posts   = @user.order("created_at DESC") 
     @data_category = @user.group("category").sum(:price)
     @data_mental = @user.group("mental").count
 
@@ -12,18 +11,26 @@ class UsersController < ApplicationController
     @this_month_sum = @user.where(created_at: Time.now.all_month).sum(:price)  # 今月
     @last_month_sum = @user.where(created_at: Time.now.ago(1.month)).sum(:price)  #先月
     
-    @food = @user.where(category: 'restaurant').sum(:price)
-    @cart = @user.where(category: 'shopping_cart').sum(:price)
-    @train = @user.where(category: 'train').sum(:price)
-    @import_contacts = @user.where(category: 'import_contacts').sum(:price)
-    @card_travel = @user.where(category: 'card_travel').sum(:price)
-    @bikes = @user.where(category: 'directions_bike').sum(:price)
-    @payment = @user.where(category: 'payment').sum(:price)
-    @star = @user.where(category: 'star').sum(:price)
+    food = @user.where(category: 'restaurant').sum(:price)
+    cart = @user.where(category: 'shopping_cart').sum(:price)
+    train = @user.where(category: 'train').sum(:price)
+    import_contacts = @user.where(category: 'import_contacts').sum(:price)
+    card_travel = @user.where(category: 'card_travel').sum(:price)
+    bikes = @user.where(category: 'directions_bike').sum(:price)
+    payment = @user.where(category: 'payment').sum(:price)
+    star = @user.where(category: 'star').sum(:price)
+    price = [food ,cart, train ,import_contacts ,card_travel ,bikes, payment, star]
+
+    category = ["restaurant", "shopping_cart", "train", "import_contacts", "card_travel", "directions_bike", "payment", "star"]
+    name = ["食費", "日用品", "交通費" ,"書籍", "ファッション", "趣味・娯楽", "カード支払" , "その他"]
+    @my_expense = category.zip(name,price)
+
+
   end
 
   def index
-    @posts = current_user.posts.order("created_at DESC").page(params[:page]).per(6)
+    @posts = @user.order("created_at DESC").page(params[:page]).per(6)
+
   end
 
   def set_user
